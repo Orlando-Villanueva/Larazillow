2924ef1ed568530d9eddf30b62758a27129681d6
OrlandoAriel <orlandovillanueva11@gmail.com>
2023-09-07 23:43:25 -0400

    Add Listing Controller, Route, Index, Show

---
+++ resources/js/Pages/Listing/Show.vue
@@ -0,0 +1,18 @@
+<template>
+  <ListingAddress :listing="listing" />
+  <div>
+    Go back to <Link :href="`/listing`">listings</Link>
+  </div>
+</template>
+
+<script setup>
+import { Link } from '@inertiajs/vue3'
+import ListingAddress from '@/Components/ListingAddress.vue'
+defineProps({
+  listing: Object,
+})
+</script>
+
+
+
+
