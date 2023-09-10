26daf0ed855defdab8a17e255dc2b43b0b583704
OrlandoAriel <orlandovillanueva11@gmail.com>
2023-09-10 14:19:52 -0400

    Revert "Update ListingController.php"
    This reverts commit 06dae4af36298cd4971b307b356cf305f232ef3c.

--- app/Http/Controllers/ListingController.php
+++ app/Http/Controllers/ListingController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 
 class ListingController extends Controller
 {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         return inertia(
@@ -31,7 +36,7 @@ class ListingController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
+     * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -57,7 +62,7 @@ class ListingController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param int $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function edit($id)
@@ -68,8 +73,8 @@ class ListingController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param \Illuminate\Http\Request $request
-     * @param int $id
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -80,7 +85,7 @@ class ListingController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param int $id
+     * @param  int  $id
      * @return \Illuminate\Http\Response
      */
     public function destroy($id)
